4557432d7e658a291a6113a72338b7729ab06497
TadeoRaffaeli <tadeo.raffa@gmail.com>
2022-11-21 20:36:00 -0300

    img updated

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { FaCartPlus } from 'react-icons/fa';
-import logo from '../logo.png';
 
 
 const CustomNavbar = () => {
@@ -9,7 +8,7 @@ const CustomNavbar = () => {
         <header>
         <Navbar id='navi'>
       <Container id='bob'>
-        <img src={logo} id='imgLogo' alt='Logo'/>
+        <img src={process.env.PUBLIC_URL + '/logo.png'} id='imgLogo' alt='Logo'/>
         <Navbar.Brand className='filtro1'>Proteinas</Navbar.Brand>
         <Navbar.Brand className='filtro2'>Creatina</Navbar.Brand>
         <Navbar.Brand className='filtro3'>Vitaminas</Navbar.Brand>
